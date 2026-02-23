ce66e42a20e2e430bd6e01c183bfcda0a54f28e0
Sami D. Munir <samidmunir@outlook.com>
2026-02-23 08:55:59 -0500

    intro to Rust modules & the pub keyword

--- Learning/rust-programming/warehouse/src/main.rs
+++ Learning/rust-programming/warehouse/src/main.rs
@@ -1,3 +1,33 @@
+mod inventory {
+    pub const FLOOR_SPACE: i32 = 10000;
+    pub const MANAGER: &str = "Sami M.";
+
+    #[derive(Debug)]
+    enum ProductCategory {
+        Ladder,
+        Hammer,
+    }
+
+    #[derive(Debug)]
+    struct Item {
+        name: String,
+        category: ProductCategory,
+        quantity: u32,
+    }
+
+    pub fn talk_to_manager() {
+        println!("\ninventory.talk_to_manager() called...");
+        println!("- Hey, {MANAGER}, how was your coffee?");
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("\nWarehouse Project");
+    println!(
+        "- This project is intended to understand Rust file/folder structuring (modules & crates)."
+    );
+
+    println!("\nThe manager of our inventory is {}.", inventory::MANAGER);
+    println!("The available floor space is {} sqft.", inventory::FLOOR_SPACE);
+    inventory::talk_to_manager();
 }
