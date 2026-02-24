42eb6101eea265a6b8968b0a2948a4487ace6742
Sami D. Munir <samidmunir@outlook.com>
2026-02-24 13:54:08 -0500

    learning Java data types, primitve vs. non-primitve, static fields/methods, main method

--- Learning/java-programming/src/main/java/com/topics/variables/Main.java
+++ Learning/java-programming/src/main/java/com/topics/variables/Main.java
@@ -2,27 +2,50 @@ package com.topics.variables;
 
 public class Main {
     /*
-        A variable is a usable container for a value.
+        Java data types are categorized into two categories:
+        - Primitive
+        - Non-primitive
+        The distinction lies between how they store data and how they are reflected within the underlying memory.
 
-        Primitive variables store simple values directly in memory (stack).
-        Reference variables hold memory addressed (stored in stack) that point to data in the heap.
+        Primitive data types are the most concrete, basic building-blocks of data in Java, allowing us to store actual value in memory. The data types are stored on the Stack. These data types cannot be null and have a fixed size in memory. It is also important to notice that each primitive data type has a corresponding Wrapper class; this is because non-primitive data types can only store objects.
 
-        Primitive Data Types
-        - int
-        - double
-        - char
-        - boolean
+        Non-primitve data types, on the other hand, store a reference or memory address to the actual object in memory. These kind of data types can be null and are not fixed in size (in memory). These data types are created on the heap, or dynamic memory.
 
-        Reference
-        - string
-        - array
-        - object
+        Primitive data types: Integers, Floating-point numbers, Characters, Boolean
+
+        Integers
+        > byte - 1 byte
+        > short - 2 bytes
+        > int - 4 bytes
+        > long - 8 bytes
+
+        Floating-point numbers
+        > double
+        > float
+
+        Characters
+        > char
+
+        Boolean
+        > boolean
+
+        Non-Primitive data types: Arrays, Strings
+    */
+   
+
+    /*
+        The main method in Java is the initial starting point of any Java program. This is possible because it is a public method which means it can be reached/called from outside of the class it is declared in. Because the main method in Java is static, it can be called by the Java Virtual Machine without having to instantiate an object of the class in which it is declared. The main method also has a return type of void, which means it does not return anything to the caller.
+
+        Static Fields & Static Methods
+        - Belong to the class rather than instances.
+        - Can be accessed without creating an object (instantiaion).
+        - Shared among all instances of a class.
     */
     public static void main(String[] args) {
         System.out.println("Variables in Java\n-----------------");
         
-        int myFavoriteNumber = 7;
-        System.out.println("\nint myFavoriteNumber = " + myFavoriteNumber);
+        int num = 7;
+        System.out.println("\nint num = " + num);
 
         double PI = 3.14159d;
         System.out.println("double PI = " + PI);
@@ -40,5 +63,14 @@ public class Main {
 
         String name = "Sami M.";
         System.out.println("String name = "  + name);
+
+        /*
+            When storing this @ character as a byte, it actually stores the value 64 (ascii value of character).
+        */
+        byte myByte = '@';
+        System.out.println("byte myByte = " + myByte);
+        
+        short myShort = 4;
+        System.out.println("short myShort = " + myShort);
     }
 }
