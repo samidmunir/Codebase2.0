2d1833088dcd2020195f5bc3a7ff88f64f6db519
Sami D. Munir <samidmunir@outlook.com>
2026-02-27 00:11:02 -0500

    intro to Rust variables

---
+++ Learning/rust-programming/variables/src/main.rs
@@ -0,0 +1,31 @@
+/*
+    Variables
+    - A variable is a name assigned to a value in the program. Serves as an identifier to what context the variable represents/stores.
+    - The Rust compiler will provide a warning for any unused variables.
+    - The Rust compiler will also infer the type of the variable based on the value stored (if not explicitly specified by the developer).
+
+    Interpolation
+
+    Positional arguments to println!()
+
+    Mutability & immutability
+
+    Rust error codes
+
+    Variable shadowing
+
+    Scopes & blocks
+
+    Constants
+
+    Type aliases
+
+    Compiler directives
+*/
+
+fn main() {
+    println!("\nVariables, Data Types, Mutability in Rust programming!");
+
+    let number_of_lines = 253;
+    println!("\nnumber_of_lines = {}", number_of_lines);
+}
